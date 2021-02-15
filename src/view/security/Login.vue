0e5468048d893439d78fec0161f34aeb395904e9
h56983577 <h56983577@126.com>
2021-02-15 16:24:14 +0800

    feature_登录页面样式

--- src/view/security/Login.vue
+++ src/view/security/Login.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
+  <div id="login_con">
     <Card>
       <Form ref="formInline" :model="loginForm" :rules="loginRule">
-        <h2>登录</h2>
-        <FormItem prop="user">
-          <Input type="text" v-model="loginForm.user" placeholder=""/>
-        </FormItem>
-        <FormItem prop="password">
-          <Input type="password" v-model="loginForm.password" placeholder=""/>
-        </FormItem>
-        <FormItem>
-          <Button type="primary" @click="handleSubmit('loginForm')">登录</Button>
-        </FormItem>
+        <h2>管理员登录</h2>
+        <br>
+        <div style="margin-left:10%;margin-right:10%">
+          <FormItem prop="user" label="用户名：">
+            <Input type="text" v-model="loginForm.user" placeholder=""/>
+          </FormItem>
+          <FormItem prop="password" label="密码：">
+            <Input type="password" v-model="loginForm.password" placeholder=""/>
+          </FormItem>
+        </div>
+        <div style="height:2vh"></div>
+        <div style="alignment: center">
+          <FormItem>
+            <Button type="warning" @click="handleSubmit('loginForm')" shape="circle" style="margin-left:30%;width:40%">
+              登录
+            </Button>
+          </FormItem>
+        </div>
       </Form>
     </Card>
   </div>
@@ -47,5 +55,19 @@ export default {
 </script>
 
 <style scoped>
+#login_con {
+  width: 100%;
+  height: 100vh;
+  background-image: url(../../assets/forbiddenCity3.jpg);
+  background-size: cover;
+  background-position: center 100%;
+}
 
+#login_con .ivu-card {
+  top: 20vh;
+  left: 37.5%;
+  height: 45vh;
+  width: 25%;
+  background-color: #f8f8f9;
+}
 </style>
