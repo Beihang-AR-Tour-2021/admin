e66f73b5b89bc3095fa9f482b9ff700d4230b94a
h56983577 <h56983577@126.com>
2021-02-15 17:06:05 +0800

    feature_导航栏

--- src/view/admin/CreateZone.vue
+++ src/view/admin/CreateZone.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div style="height: 100%">
+    <Layout :style="{padding: '0 24px 24px',height: '100%'}">
+      <Breadcrumb :style="{margin: '24px 0'}">
+        <BreadcrumbItem>后台管理</BreadcrumbItem>
+        <BreadcrumbItem>添加景区</BreadcrumbItem>
+      </Breadcrumb>
+      <Card :style="{padding: '24px', minHeight: '90%', background: '#fff'}">
+        Content
+      </Card>
+    </Layout>
+  </div>
 </template>
 
 <script>
--- src/view/admin/Nav.vue
+++ src/view/admin/Nav.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <div class="layout">
+    <Layout id="admin_con">
+      <Header>
+        <h1 style="color:white">智游</h1>
+      </Header>
+      <Layout>
+        <Sider hide-trigger :style="{background: '#fff'}">
+          <Menu active-name="2" theme="light" width="auto">
+            <MenuItem name="2" to="/admin/createZone">
+              添加景区
+            </MenuItem>
+          </Menu>
+        </Sider>
+        <Layout>
+          <router-view/>
+        </Layout>
+      </Layout>
+    </Layout>
+  </div>
 </template>
 
 <script>
@@ -9,5 +27,24 @@ export default {
 </script>
 
 <style scoped>
+#admin_con {
+  width: 100%;
+  height: 100vh;
+}
 
+#admin_con .ivu-layout-header {
+  background: #ff9900;
+}
+
+#admin_con .ivu-layout-sider {
+  background: #FFBB66 !important;
+}
+
+#admin_con .ivu-menu {
+  background: #FFDDAA !important;
+  color: white;
+}
+
+#admin_con .ivu-menu-item {
+}
 </style>
